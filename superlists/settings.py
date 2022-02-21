8097af750086b43fefaa0f5c567c257646d71fee
pavko <pylypcukp@gmail.com>
2022-02-21 11:27:23 +0100

    Enabled admin once again

--- superlists/settings.py
+++ superlists/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'lists.apps.ListsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
