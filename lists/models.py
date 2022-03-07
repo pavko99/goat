20d619cd6bfa896dce87c98b153a2f5e6b5dc880
Pavel Pylypcuk <pavel.pylypcuk@anect.com>
2022-03-07 19:02:29 +0100

    reversing test changes

--- lists/models.py
+++ lists/models.py
@@ -9,4 +9,3 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
-    print("Fart")
